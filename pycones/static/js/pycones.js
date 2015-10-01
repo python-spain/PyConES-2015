f843d5149b972a5693c5cc7952dce16c5cda42fb
Marcos Gabarda <marcosgabarda@gmail.com>
2015-10-01 11:46:53 +0200

    minor design issues

--- pycones/static/js/pycones.js
+++ pycones/static/js/pycones.js
@@ -5,13 +5,18 @@
     $(document).ready(function(){
         $(".slot-inner").on("click", function(event) {
             var description = $("#slot-description-" + $(this).data("slot"));
-            //if (description.hasClass("hidden")) {
-            //    description.removeClass("hidden");
-            //}
-            //else {
-            //    description.addClass("hidden");
-            //}
             description.fadeToggle();
+            var ad = $(this).find(".fa-angle-down");
+            var au = $(this).find(".fa-angle-up");
+            if (ad.length > 0) {
+                ad.removeClass("fa-angle-down");
+                ad.addClass("fa-angle-up");
+            }
+            if (au.length > 0) {
+                au.addClass("fa-angle-down");
+                au.removeClass("fa-angle-up");
+            }
+
         });
     });
 })();
