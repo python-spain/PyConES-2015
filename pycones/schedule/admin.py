8f32216f5d5df1081b80f716d2c2c6fbe72ef9be
Marcos Gabarda <marcosgabarda@gmail.com>
2015-10-02 19:01:44 +0200

    default_room

--- pycones/schedule/admin.py
+++ pycones/schedule/admin.py
@@ -2,17 +2,26 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from schedule.models import Schedule, Day, Room, SlotKind, Slot, SlotRoom, Presentation
+
+
+class SlotAdmin(ModelAdmin):
+    list_display = ("id", "day", "start", "end", "kind", "default_room", "current_room")
+
+    def current_room(self, instance):
+        try:
+            return instance.rooms[0]
+        except IndexError:
+            return None
+
 
 admin.site.register(Schedule)
 admin.site.register(Day)
 admin.site.register(Room)
 admin.site.register(SlotKind)
-admin.site.register(
-    Slot,
-    list_display=("id", "day", "start", "end", "kind", "default_room")
-)
+admin.site.register(Slot, SlotAdmin)
 admin.site.register(
     SlotRoom,
     list_display=("id", "slot", "room")
